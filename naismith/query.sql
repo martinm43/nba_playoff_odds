d8e067c5c67b9ade369d81d75ed3fe328d24c0eb
Martin Miller <miller.m.43@gmail.com>
2017-07-26 18:08:18 -0400

    Carraige returns now acceptable in sql source files

--- naismith/query.sql
+++ naismith/query.sql
@@ -1,1 +1,6 @@
-select case when away_standard_id=28 then away_pts-home_pts+2 when home_standard_id=28 then home_pts-away_pts-2 end from nba_py_api_data where (away_standard_id=28 or home_standard_id=28);
+select case 
+when away_standard_id=28 then away_pts-home_pts+2 
+when home_standard_id=28 then home_pts-away_pts-2 
+end 
+from nba_py_api_data 
+where (away_standard_id=28 or home_standard_id=28);
