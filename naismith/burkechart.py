7bbf658d69d1cf3eb4084399e9d6caccf2294446
MA Miller <miller.m.43@gmail.com>
2017-08-14 20:11:06 -0400

    charting team performance experiment

--- naismith/burkechart.py
+++ naismith/burkechart.py
@@ -16,8 +16,8 @@ def runningMeanFast(x, N):
     return np.convolve(x, np.ones((N,))/N)[(N-1):]
 
 #set start date and date related variables
-start_date=epochtime('Dec 1 2015')
-end_date=epochtime('May 1 2016')
+start_date=epochtime('Dec 1 2014')
+end_date=epochtime('May 1 2015')
 calc_date=start_date
 daysecs=24*60*60
 monthsecs=daysecs*30
@@ -26,20 +26,19 @@ monthsecs=daysecs*30
 all_burke_calcs=[]
 
 while calc_date < end_date:
-    temp_burke_calc=burke_calc(games_query(calc_date-monthsecs,calc_date))
+    temp_burke_calc=burke_calc(games_query(start_date-daysecs*60,calc_date))
     temp_burke_calc.tolist()
     all_burke_calcs.append(temp_burke_calc)
     calc_date+=daysecs
 
 all_burke_calcs=np.asarray(all_burke_calcs)
-#Not used, too jerky at end
-#ave_burke_calcs=np.zeros_like(all_burke_calcs)
-#
-##calculate ave_burke_calcs
-#for i in range(0,len(all_burke_calcs[0,:])):
-#    ave_burke_calcs[:,i]=runningMeanFast(all_burke_calcs[:,i],ave_period)
+ave_burke_calcs=np.zeros_like(all_burke_calcs)
+
+#calculate ave_burke_calcs
+for i in range(0,len(all_burke_calcs[0,:])):
+    ave_burke_calcs[:,i]=runningMeanFast(all_burke_calcs[:,i],ave_period)
     
-plot_team_id=3
+plot_team_id=8
 plt.plot(all_burke_calcs[:,plot_team_id])
 plt.plot(ave_burke_calcs[:,plot_team_id])
 plt.ylabel('team Burke score')
