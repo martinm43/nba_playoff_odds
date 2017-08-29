69c0fb2ec46d0db2256b8c2777925b4502e37d1d
Ubuntu <ubuntu@ip-172-31-11-228.us-west-2.compute.internal>
2017-08-29 23:13:55 +0000

    correcting file errors

--- naismith/regress.py
+++ naismith/regress.py
@@ -6,17 +6,8 @@ import statsmodels.api as sm
 import numpy as np
 get_ipython().magic(u'ls')
 df = pd.read_csv("burke_results.csv",names=["score","diff"])
-df
-pd.describe()
 df.describe()
 df['away_win'] = df['score']>0
-df
-df
-df.describe
-df
-df['away_win']=df['score']>0
-df
-df=df
 df
 fullset=df[['diff','away_win']]
 fullset
