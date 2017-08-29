575e1e58b3cfe14f7a56e3700c796c7010ff66ab
Ubuntu <ubuntu@ip-172-31-11-228.us-west-2.compute.internal>
2017-08-29 22:39:34 +0000

    regression complete but need ro study data

--- naismith/regress.py
+++ naismith/regress.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+get_ipython().magic(u'load regress.py')
+# %load regress.py
 import pandas as pd
 import statsmodels.api as sm
 import numpy as np
@@ -9,3 +11,16 @@ pd.describe()
 df.describe()
 df['away_win'] = df['score']>0
 df
+df
+df.describe
+df
+df['away_win']=df['score']>0
+df
+df=df
+df
+fullset=df[['diff','away_win']]
+fullset
+logit=sm.Logit(fullset['away_win'], fullset[['diff']])
+result=logit.fit()
+print result.summary()
+get_ipython().magic(u'save regress 1-15')
