cbc7fd4f04841f8adb3fa51ff6ade177bb3f7cf9
martinm43 <miller.m.43@gmail.com>
2017-09-28 09:01:32 -0400

    checking data in nba_py_api_data

---
+++ naismith/nba_py_api_data_check.py
@@ -0,0 +1,80 @@
+# coding: utf-8
+get_ipython().magic(u'ls ')
+from nba_data_models import *
+s=NBAPyApiData.Select()
+s=NbaPyApiData.Select()
+s=NbaPyApiData.select()
+for i in s:
+    print i.id
+    
+for i in s:
+    print i.id
+    
+for i in s:
+    print away_pts,away_team,home_pts,home_team
+    
+for i in s:
+    print i.away_pts,i.away_team,i.home_pts,i.home_team
+    
+    
+for i in s:
+    print i.day_datetime,i.away_pts,i.away_team,i.home_pts,i.home_team
+    
+    
+get_ipython().magic(u'ls ')
+from access_nba_data import epochtime
+s=NbaPyApiData.select().where(i.day_datetime<=epochtime('Apr 12 2017'))
+s
+s.count()
+s
+for i in s:
+    print i.day_datetime,i.away_pts,i.away_team,i.home_pts,i.home_team
+    
+    
+for i in s:
+    print i.day_datetime,i.away_pts,i.away_team,i.home_pts,i.home_team
+    
+s=NbaPyApiData.select().where(NbaPyApiData.day_datetime<=epochtime('Apr 12 2017'))
+for i in s:
+    print i.day_datetime,i.away_pts,i.away_team,i.home_pts,i.home_team
+    
+epochtime('Apr 12 2017')
+for i in s:
+    print i.day_datetime,i.away_pts,i.away_standard,i.home_pts,i.home_standard
+    
+for i in s:
+    print i.day_datetime,i.away_pts,i.away_standard,i.home_pts,i.home_standard,(away_pts_ot1 or home_pts_ot1)
+     
+for i in s:
+    print i.day_datetime,i.away_pts,i.away_standard,i.home_pts,i.home_standard,(i.away_pts_ot1 or i.home_pts_ot1)
+    
+     
+for i in s:
+    print i.day_datetime,i.away_pts,i.away_standard,i.home_pts,i.home_standard,(i.away_pts_ot1 or i.home_pts_ot1)
+    
+for i in s:
+    print i.day_datetime,i.away_pts,i.away_standard,i.home_pts,i.home_standard,(i.away_pts_ot1 > 0 and i.home_pts_ot1>0)
+    
+    
+for i in s:
+    print i.day_datetime,i.away_pts,i.away_standard,i.home_pts,i.home_standard,(i.away_pts_ot1 > 0 and i.home_pts_ot1>0),i.away_team_abbreviation,i.home_team_abbreviatiom
+    
+    
+for i in s:
+    print i.day_datetime,i.away_pts,i.away_standard,i.home_pts,i.home_standard,(i.away_pts_ot1 > 0 and i.home_pts_ot1>0),i.away_team_abbreviation,i.home_team_abbreviation
+    
+    
+    
+j=1
+for i in s:
+    print i.day_datetime,i.away_pts,i.away_standard,i.home_pts,i.home_standard,(i.away_pts_ot1 > 0 and i.home_pts_ot1>0),i.away_team_abbreviation,i.home_team_abbreviation
+   p
+  
+    
+    
+j=1
+for i in s:
+    print i.day_datetime,i.away_pts,i.away_standard,i.home_pts,i.home_standard,(i.away_pts_ot1 > 0 and i.home_pts_ot1>0),i.away_team_abbreviation,i.home_team_abbreviation
+    print j
+    j+=1
+    
