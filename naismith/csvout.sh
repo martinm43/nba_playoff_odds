67eabe59f737d41dd7016355a9817697a8d35d86
MA Miller <miller.m.43@gmail.com>
2017-09-28 23:14:03 -0400

    First phase of 2017 cleanup. Added in historical data. Shifting away from 'strgen' and using ORMs

--- naismith/csvout.sh
+++
@@ -1,4 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/sh
-#Change the above to $PATH for the relevant Linux system
-sqlite3 nba_data_test.sqlite < csvout.txt
-cp trimatch.csv sqlcsv
---
+++ naismith/teamind.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 28 22:59:43 2017
+
+@author: martin
+"""
+
+def teamind ( str ):
+	if str=='Atlanta Hawks':
+		return 1
+	if str=='Boston Celtics':
+		return 2
+	if str=='Brooklyn Nets':
+		return 3
+	if str=='Charlotte Hornets':
+		return 4
+	if str=='Chicago Bulls':
+		return 5
+	if str=='Cleveland Cavaliers':
+		return 6
+	if str=='Dallas Mavericks':
+		return 7
+	if str=='Denver Nuggets':
+		return 8
+	if str=='Detroit Pistons':
+		return 9
+	if str=='Golden State Warriors':
+		return 10
+	if str=='Houston Rockets':
+		return 11
+	if str=='Indiana Pacers':
+		return 12
+	if str=='Los Angeles Clippers':
+		return 13
+	if str=='Los Angeles Lakers':
+		return 14
+	if str=='Memphis Grizzlies':
+		return 15
+	if str=='Miami Heat':
+		return 16
+	if str=='Milwaukee Bucks':
+		return 17
+	if str=='Minnesota Timberwolves':
+		return 18
+	if str=='New Orleans Pelicans':
+		return 19
+	if str=='New York Knicks':
+		return 20
+	if str=='Oklahoma City Thunder':
+		return 21
+	if str=='Orlando Magic':
+		return 22
+	if str=='Philadelphia 76ers':
+		return 23
+	if str=='Phoenix Suns':
+		return 24
+	if str=='Portland Trail Blazers':
+		return 25
+	if str=='Sacramento Kings':
+		return 26
+	if str=='San Antonio Spurs':
+		return 27
+	if str=='Toronto Raptors':
+		return 28
+	if str=='Utah Jazz':
+		return 29
+	if str=='Washington Wizards':
+		return 30
+	else:
+		return str
