fixed error in pythagorean calculation optional dates causing solutions to be 0

naismith/loop_monte_carlo.py
@@ -78,7 +78,7 @@ winrows = np.asarray(winrows)
 all_sims=[]
 
 #custom simulation number
-ite=200000
+ite=250000
 
 for i in range (ite):
 	#print '{:.2%}'.format(i/ite) + ' Percent Complete'