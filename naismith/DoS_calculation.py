2cf35b307a982a520b0d30b2bcbd80de795181fc
MA Miller <miller.m.43@gmail.com>
2017-10-26 19:04:18 -0400

    removing deprecated old statements

--- naismith/DoS_calculation.py
+++ naismith/DoS_calculation.py
@@ -5,12 +5,6 @@ import numpy as np
 from dbtools.nba_data_models import NbaPyApiData, BballrefScores
 from dbtools.access_nba_data import epochtime, stringtime
 
-#defaults
-init_rank=750 #what a "neutral team" is valued at
-dos_factor=10 #factor applied to change value based on expected performance vs actual
-def_env_factor=0.6 #environment factor for away team
-#The trickiest factor
-def_expect_factor=0.5 #affects sigmoid plot of -1 to +1 outcomes based on quality diff.
 #how much does talent disparity affect expected "win ratio"?
 #local function definitions
 def dos(pts_a,pts_b):
@@ -22,8 +16,17 @@ def expected_dos(rank_a,rank_b,env_factor=def_env_factor,expect_factor=def_expec
 #arrays
 ranks=np.ones(30)*init_rank
 
+##Function Definition Here
+
+#defaults
+init_rank=750 #what a "neutral team" is valued at
+dos_factor=3.5 #factor applied to change value based on expected performance vs actual
+def_env_factor=0.6 #environment factor for away team
+#The trickiest factor
+def_expect_factor=2 #affects sigmoid plot of -1 to +1 outcomes based on quality diff.
+
 start_date=epochtime('Oct 1 1997')
-end_date=epochtime('May 1 2001')
+end_date=epochtime('May 1 2017')
 
 s=NbaPyApiData.select().where(NbaPyApiData.day_datetime>=start_date,\
                                 NbaPyApiData.day_datetime<=end_date)
@@ -38,23 +41,6 @@ s_array=[(i.away_standard,i.away_pts,i.home_standard,i.home_pts) for i in s]
 s_array=[(i.away_team_id,i.away_pts,i.home_team_id,i.home_pts) for i in s_b]
 #check
 pprint(s_array)
-
-
-#for i in s:
- #   if i.away_pts>0 and i.home_pts>0:
-  #      away_rank=ranks[i.away_standard-1]
-   #     home_rank=ranks[i.home_standard-1]
-    #    expected_away_diff=expected_dos(away_rank,home_rank)
-     #   expected_home_diff=expected_dos(home_rank,away_rank)
-        #print([expected_away_diff,expected_home_diff])
-      #  away_diff=dos_factor*(dos(i.away_pts,i.home_pts)-expected_away_diff)
-       # home_diff=dos_factor*(dos(i.home_pts,i.away_pts)-expected_home_diff)
-	#print([away_diff,home_diff])
-        #ranks[i.away_standard-1]=away_rank+away_diff
-        #ranks[i.home_standard-1]=home_rank+home_diff
-        #print i.away_team_city_name,i.home_team_city_name
-        #for j in enumerate(ranks):
-            #print [j[0]+1,j[1]]
 
 for i in s_array:
     if i[1]>0 and i[3]>0:
