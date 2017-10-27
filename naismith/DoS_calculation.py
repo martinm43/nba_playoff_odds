1a633e4d8c539a0eea5fa21bd4a3a56da15af01e
MA Miller <miller.m.43@gmail.com>
2017-10-26 21:12:35 -0400

    error based tuning

--- naismith/DoS_calculation.py
+++ naismith/DoS_calculation.py
@@ -5,27 +5,25 @@ import numpy as np
 from dbtools.nba_data_models import NbaPyApiData, BballrefScores
 from dbtools.access_nba_data import epochtime, stringtime
 
-#how much does talent disparity affect expected "win ratio"?
 #local function definitions
 def dos(pts_a,pts_b):
     return (pts_a-pts_b)/(pts_a+pts_b)
 
-def expected_dos(rank_a,rank_b,env_factor=def_env_factor,expect_factor=def_expect_factor):
+def expected_dos(rank_a,rank_b,env_factor=3,expect_factor=0.05):
     return -1+expect_factor/(1+np.exp(rank_a-rank_b-env_factor))
+
+#defaults
+init_rank=1000 #what a "neutral team" is valued at
+dos_factor=20 #factor applied to change value based on expected performance vs actual
+def_env_factor=.75 #environment factor for away team
+def_expect_factor=1.0 #affects sigmoid plot of -1 to +1 outcomes based on quality diff.
 
 #arrays
 ranks=np.ones(30)*init_rank
 
-##Function Definition Here
 
-#defaults
-init_rank=750 #what a "neutral team" is valued at
-dos_factor=3.5 #factor applied to change value based on expected performance vs actual
-def_env_factor=0.6 #environment factor for away team
-#The trickiest factor
-def_expect_factor=2 #affects sigmoid plot of -1 to +1 outcomes based on quality diff.
 
-start_date=epochtime('Oct 1 1997')
+start_date=epochtime('Oct 1 2016')
 end_date=epochtime('May 1 2017')
 
 s=NbaPyApiData.select().where(NbaPyApiData.day_datetime>=start_date,\
@@ -36,22 +34,25 @@ s_b=BballrefScores.select().where(BballrefScores.datetime>=start_date,\
 
 
 #nba py api version
-s_array=[(i.away_standard,i.away_pts,i.home_standard,i.home_pts) for i in s]
+#s_array=[(i.away_standard,i.away_pts,i.home_standard,i.home_pts) for i in s]
 #bballref version
 s_array=[(i.away_team_id,i.away_pts,i.home_team_id,i.home_pts) for i in s_b]
-#check
-pprint(s_array)
+
+
+error=0
 
 for i in s_array:
     if i[1]>0 and i[3]>0:
         away_rank=ranks[i[0]-1]
         home_rank=ranks[i[2]-1]
-        expected_away_diff=expected_dos(away_rank,home_rank)
-        expected_home_diff=expected_dos(home_rank,away_rank)
+        expected_away_diff=expected_dos(away_rank,home_rank,env_factor=def_env_factor,expect_factor=def_expect_factor)
+        expected_home_diff=expected_dos(home_rank,away_rank,env_factor=def_env_factor,expect_factor=def_expect_factor)
         away_diff=dos_factor*(dos(i[1],i[3])-expected_away_diff)
         home_diff=dos_factor*(dos(i[3],i[1])-expected_home_diff)
+        error+=(dos(i[1],i[3])-expected_away_diff)**2+(dos(i[3],i[1])-expected_home_diff)**2
         ranks[i[0]-1]=away_rank+away_diff
         ranks[i[2]-1]=home_rank+home_diff
         for j in enumerate(ranks):
             print [j[0]+1,j[1]]
     
+#print error
---
+++ naismith/DoS_optimization.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+from __future__ import division
+from pprint import pprint
+import numpy as np
+from dbtools.nba_data_models import NbaPyApiData, BballrefScores
+from dbtools.access_nba_data import epochtime, stringtime
+
+#local function definitions
+def dos(pts_a,pts_b):
+    return (pts_a-pts_b)/(pts_a+pts_b)
+
+def expected_dos(rank_a,rank_b,env_factor=3,expect_factor=0.05):
+    return -1+expect_factor/(1+np.exp(rank_a-rank_b-env_factor))
+
+def DoS_calculation_error(s_array,def_env_factor, def_expect_factor):
+    
+    #defaults
+    init_rank=1000 #what a "neutral team" is valued at
+    dos_factor=30 #factor applied to change value based on expected performance vs actual
+    
+    #arrays
+    ranks=np.ones(30)*init_rank
+
+    error=0
+
+    for i in s_array:
+        if i[1]>0 and i[3]>0:
+            away_rank=ranks[i[0]-1]
+            home_rank=ranks[i[2]-1]
+            expected_away_diff=expected_dos(away_rank,home_rank,env_factor=def_env_factor,expect_factor=def_expect_factor)
+            expected_home_diff=expected_dos(home_rank,away_rank,env_factor=def_env_factor,expect_factor=def_expect_factor)
+            away_diff=dos_factor*(dos(i[1],i[3])-expected_away_diff)
+            home_diff=dos_factor*(dos(i[3],i[1])-expected_home_diff)
+            error+=(dos(i[1],i[3])-expected_away_diff)**2+(dos(i[3],i[1])-expected_home_diff)**2
+            ranks[i[0]-1]=away_rank+away_diff
+            ranks[i[2]-1]=home_rank+home_diff
+
+    return error
+
+if __name__=='__main__':
+    #input variables to function
+    start_date=epochtime('Oct 1 2009')
+    end_date=epochtime('May 1 2014')
+    s_b=BballrefScores.select().where(BballrefScores.datetime>=start_date,\
+                                BballrefScores.datetime<=end_date)
+    #Convert into universal format
+    s_array=[(i.away_team_id,i.away_pts,i.home_team_id,i.home_pts) for i in s_b]
+
+    test_values=np.linspace(0.6,1.0,100)
+    #Crude investigation into the possible ranges of "ideal values"
+    errors=[(i,DoS_calculation_error(s_array,i,1.0)) for i in test_values]
