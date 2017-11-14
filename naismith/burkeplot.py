12e7bf1442bfa7d00340f1b0642a0369edde3add
MA Miller <miller.m.43@gmail.com>
2017-11-13 21:47:42 -0500

    making changes to plot

--- naismith/burkeplot.py
+++ naismith/burkeplot.py
@@ -10,6 +10,7 @@ enddate=epochtime('Apr 15 2017')
 
 dates=np.linspace(startdate,enddate,300)
 y=[burke_month_calc(i) for i in dates]
+y=np.asarray(y)
 
 plt.title('Team Performances over 2017 season by burke_rating')
 plt.ylabel('burke_rating')
@@ -18,6 +19,9 @@ plt.xlabel('Date')
 s=ProApiTeams.select()
 team_labels=sorted([i.abbreviation for i in s])
 
-plt.legend(team_labels[:-1]) #get the "None" entry out
-plt.plot(dates,y)
+for i,team in enumerate(team_labels):
+    print team
+    plt.plot(dates,y[:,i-1],label=team)
+
+plt.legend()
 plt.show()
