0da09036c3438dbc1038bcdcb5f871d9a9d04b10
MA Miller <miller.m.43@gmail.com>
2017-11-14 22:22:54 -0500

    added in a minimum for sql sery

--- naismith/elo_query_test.sql
+++ naismith/elo_query_test.sql
@@ -1,6 +1,9 @@
-select b.id, b.away_team_id, b.away_pts-b.home_pts,
+select b.away_pts-b.home_pts,
 (select elo_rating from nba_team_elo_data 
-	where b.datetime=nba_team_elo_data.datetime
-	and b.away_team_id=nba_team_elo_data.bball_ref_id)
-	from bballref_scores as b
-	limit 5
+		where b.datetime=nba_team_elo_data.datetime
+		and b.away_team_id=nba_team_elo_data.bball_ref_id) -
+(select elo_rating from nba_team_elo_data 
+		where b.datetime=nba_team_elo_data.datetime
+		and b.home_team_id=nba_team_elo_data.bball_ref_id)
+from bballref_scores as b
+limit 100 --takes a very long time to run as written, but works as intended.
