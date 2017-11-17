8a83dc5e65d9dcd5518522f389f68a3e9a5608cf
MA Miller <miller.m.43@gmail.com>
2017-11-17 09:12:47 -0500

    one season one team

--- naismith/rest_calculations.py
+++ naismith/rest_calculations.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+# New rest calculation script. #
 from dbtools.nba_data_models import BballrefScores as bbrs
 import numpy as np
 
@@ -8,6 +9,15 @@ season_year=2018
 team_games=bbrs.select().where((bbrs.season_year==season_year) & ((bbrs.away_team_id==team) | (bbrs.home_team_id==team)))
 
 game_date_list=[[i.id,i.datetime] for i in team_games]
+game_date_array=np.asarray(game_date_list) 
 
-game_date_list[1:] #all games except the first one
-game_date_list[0][0] #first game id
+rest_array=np.zeros_like(game_date_array)
+rest_array[0,:]=np.asarray([game_date_array[0,0],24*7.0])
+
+for i in range(1,len(game_date_array)):
+    print(game_date_array[i,:])
+    rest_array[i,0]=game_date_array[i,0]
+    rest_array[i,1]=game_date_array[i,1]-game_date_array[i-1,1]
+    rest_array[i,1]=rest_array[i,1]/3600.0 #to hours
+
+print(rest_array)
