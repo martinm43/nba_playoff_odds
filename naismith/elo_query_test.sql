310e49e2ed18aa3eb6a7ec80390deab72eb9a2fc
MA Miller <miller.m.43@gmail.com>
2017-11-16 22:38:37 -0500

    elo query

--- naismith/elo_query_test.sql
+++ naismith/elo_query_test.sql
@@ -6,4 +6,4 @@ select b.away_pts-b.home_pts,
 		where b.datetime=nba_team_elo_data.datetime
 		and b.home_team_id=nba_team_elo_data.bball_ref_id)
 from bballref_scores as b
-limit 100 --takes a very long time to run as written, but works as intended.
+limit 1000 --takes a very long time to run as written, but works as intended.
