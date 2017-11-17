cc1da148e84e522bf98cb09fc2a694bf7f19be62
MA Miller <miller.m.43@gmail.com>
2017-11-16 22:46:06 -0500

    no intro values for DoS fitting calc

--- naismith/elo_query_test.sql
+++ naismith/elo_query_test.sql
@@ -1,9 +1,11 @@
-select b.away_pts-b.home_pts,
+select b.id,b.away_pts-b.home_pts,
 (select elo_rating from nba_team_elo_data 
 		where b.datetime=nba_team_elo_data.datetime
-		and b.away_team_id=nba_team_elo_data.bball_ref_id) -
+		and b.away_team_id=nba_team_elo_data.bball_ref_id
+		and elo_rating <> 1500) -
 (select elo_rating from nba_team_elo_data 
 		where b.datetime=nba_team_elo_data.datetime
-		and b.home_team_id=nba_team_elo_data.bball_ref_id)
-from bballref_scores as b
-limit 1000 --takes a very long time to run as written, but works as intended.
+		and b.home_team_id=nba_team_elo_data.bball_ref_id
+		and elo_rating <> 1500)
+from bballref_scores as b;
+--takes a very long time to run as written, but works as intended.
