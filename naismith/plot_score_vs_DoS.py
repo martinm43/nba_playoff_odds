e9a3e01a19715895cff46a3c64c293f1591fc69b
MA Miller <miller.m.43@gmail.com>
2017-12-06 21:21:06 -0500

    plot score vs DoS function

--- naismith/plot_score_vs_DoS.py
+++ naismith/plot_score_vs_DoS.py
@@ -8,4 +8,4 @@ import pandas as pd
 games=pd.read_csv('results.csv',names=['id','score','dDoS'])
 import matplotlib.pyplot as plt
 plt.plot(games['dDoS'],games['score'],'o')
-plt.show()
+pylab.savefig('score_vs_dos.png')
