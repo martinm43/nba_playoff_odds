621885dcf26cb830b54a1f37430f0d250bc69310
MA Miller <miller.m.43@gmail.com>
2017-12-24 01:05:05 -0400

    Adding in a fix for "No values found" (i.e. one of the queried values is None) Also removed all references to srscalc in preparation for a purge of SRScalc related content

--- naismith/ratings_calculations.py
+++ naismith/ratings_calculations.py
@@ -6,7 +6,6 @@
 #MAM
 
 import csv,os
-from analytics.srscalc_script import srscalc
 from string_conversion_tools import team_abbreviation
 import sqlite3
 from dbtools.access_nba_data import epochtime
@@ -39,23 +38,20 @@ c=conn.cursor()
 #Convert the data into integers (this will not be necessary if using DB data)
 srsdata=[[int(m) for m in l] for l in srsdata]
 
-#Calculate SRS via blow-up proof approximation.
-srsdicts=srscalc(srsdata)
-
 if burke_solve==1:
     #Calculate Burke SRS
     analysis_start_date=raw_input('Enter start date for Burke-type analysis (e.g. Jan 1 2016): ')
     analysis_end_date=raw_input('Enter end date for Burke-type analysis (e.g. Feb 1 2016): ')
     max_MOV=float(raw_input('Enter max margin of victory for Burke-type analysis: '))
-#    home_team_adv=float(raw_input('Enter presumed home team advantage: '))
+    home_team_adv=float(raw_input('Enter presumed home team advantage: '))
     analysis_start_date=epochtime(analysis_start_date)
     analysis_end_date=epochtime(analysis_end_date)
     nba_api_srsdata_query_str='SELECT away_standard_id, away_PTS, home_standard_id, home_PTS\
                              from nba_py_api_data WHERE day_datetime >= '+str(analysis_start_date)+' AND day_datetime <= '+str(analysis_end_date)
     nba_api_srsdata=c.execute(nba_api_srsdata_query_str).fetchall()
     srsdata=nba_api_srsdata
-    burke_data=[[s[2],s[0],s[3],s[1]] for s in srsdata]
-    burkelist=burke_calc(burke_data,impmode=None,max_MOV=max_MOV) #,home_team_adv=home_team_adv)
+    burke_data=[[s[2],s[0],s[3],s[1]] for s in srsdata if s[1] is not None]
+    burkelist=burke_calc(burke_data,impmode=None,max_MOV=max_MOV,home_team_adv=home_team_adv)
     burkelist=[[b] for b in burkelist]
 else:
     burkelist=None
@@ -66,17 +62,6 @@ if burkelist!=None:
       print('Burke rating of team '+team_abbreviation(i+1)+' is '+str(burke_value[0]))
 else:
   print('Burke calculations not performed, skipping')
-
-#SRS_vector for writing to file
-SRS_vector=[[s['srs']] for s in srsdicts]
-
-#write it out.
-csvfile_out = open(wkdir+'SRS_vector.csv','wb')
-csvwriter = csv.writer(csvfile_out)
-for row in SRS_vector:
-	#Only need to print the visiting and home team scores and names.
-	csvwriter.writerow(row)
-csvfile_out.close()
 
 if burke_solve==1:
     #write out burke vector
