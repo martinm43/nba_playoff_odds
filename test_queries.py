5d321355ee3ae5efcb18a6fb6ca347c854ed1a37
martin <MA Miller>
2018-08-16 22:33:54 -0300

    updating test queries

--- test_queries.py
+++ test_queries.py
@@ -4,12 +4,16 @@ from datetime import datetime
 from pprint import pprint
 #Query imports
 from nba_database.queries import games_query
+from nba_database.nba_data_models import BballrefScores as Game
 #Analytics imports
 from analytics.SRS import SRS
+from analytics.pythag import pythagorean_wins
 
 start_datetime = datetime(1997,01,01)
 end_datetime = datetime(2016,03,31)
 
 games_list=games_query(start_datetime,end_datetime)
 
-pprint(SRS(games_list))
+#pprint(SRS(games_list))
+
+print(pythagorean_wins(Game,28,2002))
