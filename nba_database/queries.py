96304599047c59057dc41b5b9c79b08b2efb0525
martin <MA Miller>
2018-08-17 10:30:40 -0300

    Pythagorean win expectation now confirmed working.

--- nba_database/queries.py
+++ nba_database/queries.py
@@ -6,27 +6,19 @@
 from nba_data_models import BballrefScores as Game
 import time, datetime
 
-def epochtime(str_time):
+###################
+# Time Conversion #
+###################
+
+def epochtime(datetime_obj):
   """
   Convert time in MON DAY YEAR format to a UNIX timestamp
   """
-  datetime_obj=datetime.datetime.strptime(str_time,"%b %d %Y")
   return time.mktime(datetime_obj.timetuple())
 
-def games_query(start_datetime,end_datetime):
-    """
-    Input: datetime objects
-    Output: [away_team, away_pts, home_team, home_pts] list
-    """
-    start_epochtime=time.mktime(start_datetime.timetuple())
-    end_epochtime=time.mktime(end_datetime.timetuple())
-    played_games = Game.select().where(
-        Game.datetime < end_epochtime,
-        Game.datetime > start_epochtime).order_by(Game.datetime)
-
-    played_games = [[g.away_team_id, g.away_pts, g.home_team_id, g.home_pts]
-                    for g in played_games]
-    return played_games
+#####################
+# String Conversion #
+#####################
 
 def team_abbreviation(team_alphabetical_id):
     """
@@ -37,4 +29,55 @@ def team_abbreviation(team_alphabetical_id):
       where(ProApiTeams.bball_ref == team_alphabetical_id)
     s_result = s_query[0]
     return s_result.abbreviation
+
+################################
+# Getting Information On Games #
+################################
+
+def games_query(start_datetime,end_datetime):
+    """
+    Input: datetime objects
+    Output: [away_team, away_pts, home_team, home_pts] list
+    """
+    start_epochtime=epochtime(start_datetime)
+    end_epochtime=epochtime(end_datetime)
+    played_games = Game.select().where(
+        Game.datetime < end_epochtime,
+        Game.datetime > start_epochtime).order_by(Game.datetime)
+
+    played_games = [[g.away_team_id, g.away_pts, g.home_team_id, g.home_pts]
+                    for g in played_games]
+    return played_games
+
+def games_won_query(played_games,return_format="list"):
+    """
+    Input: [away_team, away_pts, home_team, home_pts] list
+    Output: a list of lists, a list, or a matrix
+    """
+    winlist = [x[0] if x[1] > x[3] else x[2] for x in played_games]
+    winrows = []
+    if return_format == 'list_of_lists':
+        winlist = [x[0] if x[1] > x[3] else x[2] for x in played_games]
+        winrows = []
+        for i in range(1, 31):
+            winrows.append([winlist.count(i)])
+        return_value = winrows
+    elif return_format == 'list':
+        winlist = [x[0] if x[1] > x[3] else x[2] for x in played_games]
+        winrows = []
+        for i in range(1, 31):
+            winrows.append(winlist.count(i))
+        return_value = winrows
+    elif return_format == 'matrix':
+        win_matrix = np.zeros((30, 30))
+        for x in played_games:
+            if x[1] > x[3]:
+                win_matrix[x[0] - 1, x[2] - 1] += 1
+            elif x[3] > x[1]:
+                win_matrix[x[2] - 1, x[0] - 1] += 1
+        return win_matrix
+    else:
+        print('invalid option')
+        return_value = 0
+    return return_value
 
--- test_queries.py
+++ test_queries.py
@@ -3,20 +3,21 @@ from __future__ import division, print_function
 from datetime import datetime
 from pprint import pprint
 #Query imports
-from nba_database.queries import games_query, team_abbreviation
+from nba_database.queries import games_query, team_abbreviation, games_won_query, epochtime
 from nba_database.nba_data_models import BballrefScores as Game
-from nba_database.supports import epochtime
 #Analytics imports
 from analytics.SRS import SRS
 from analytics.pythag import pythagorean_wins, league_pythagorean_wins
 
-start_datetime = datetime(1997,10,01)
-end_datetime = datetime(1998,04,30)
+#Query Testing
+start_datetime = datetime(2007,10,01)
+end_datetime = datetime(2008,04,30)
 
 games_list=games_query(start_datetime,end_datetime)
 
-#pprint(SRS(games_list))
+pprint(games_won_query(games_list))
 
+#Pythagorean Wins Testing
 lpw_results = league_pythagorean_wins(Game,mincalcdatetime=epochtime(start_datetime),\
             maxcalcdatetime=epochtime(end_datetime))
 
