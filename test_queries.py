0ddac6d8b20805bdb2e0d9c29a8fece3c9e2f681
martin <MA Miller>
2018-08-16 22:12:53 -0300

    basic work is now complete

--- test_queries.py
+++ test_queries.py
@@ -1,8 +1,15 @@
-from nba_database.queries import games_query
+from __future__ import division, print_function
+#Standard imports
 from datetime import datetime
 from pprint import pprint
+#Query imports
+from nba_database.queries import games_query
+#Analytics imports
+from analytics.SRS import SRS
 
 start_datetime = datetime(2016,01,01)
 end_datetime = datetime(2016,03,31)
 
-pprint(games_query(start_datetime,end_datetime))
+games_list=games_query(start_datetime,end_datetime)
+
+pprint(SRS(games_list))
