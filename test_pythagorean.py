948700bf54e3d082bac61d451c792803bc94de55
martin <MA Miller>
2018-08-19 19:38:22 -0400

    removed debug print statements from pythagorean programming file; renamed file to be more appropriate

--- test_pythagorean.py
+++ test_pythagorean.py
@@ -2,6 +2,8 @@ from __future__ import division, print_function
 #Standard imports
 from datetime import datetime
 from pprint import pprint
+#Third Party Imports
+from tabulate import tabulate
 #Query imports
 from nba_database.queries import games_query, team_abbreviation, games_won_query, epochtime
 from nba_database.nba_data_models import BballrefScores as Game
@@ -15,11 +17,22 @@ end_datetime = datetime(2008,04,30)
 
 games_list=games_query(start_datetime,end_datetime)
 
-pprint(games_won_query(games_list, return_format="matrix"))
+#pprint(games_won_query(games_list, return_format="matrix"))
 
 #Pythagorean Wins Testing
 lpw_results = league_pythagorean_wins(Game,mincalcdatetime=epochtime(start_datetime),\
             maxcalcdatetime=epochtime(end_datetime))
 
-lpw_results = [[team_abbreviation(x[0]),x[1]] for x in lpw_results]
-pprint(lpw_results)
+lpw_tuples = [(team_abbreviation(x[0]),round(x[1],0)) for x in lpw_results]
+results_table = tabulate(
+        lpw_tuples,
+        headers=[
+            'Team',
+            'Pythag. Wins'],
+        tablefmt='rst',
+        numalign='left')
+
+print("Pythagorean Win Expectations \n"+\
+        "Based on Games Played Between: "+start_datetime.strftime('%b %d %Y')+\
+        " and "+end_datetime.strftime('%b %d %Y'))
+print(results_table)
