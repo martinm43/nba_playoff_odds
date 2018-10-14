5b2f79531fb2be816998233b459989999691b44e
martin <MA Miller>
2018-10-14 19:13:01 -0400

    Oriented tick marks on graph, automated 'get teams in division' process

--- test_odds_plotting.py
+++ test_odds_plotting.py
@@ -6,6 +6,7 @@ import numpy as np
 from mcss_nba import playoff_odds_calc
 from pprint import pprint
 from nba_database.queries import team_abbreviation
+from nba_database.nba_data_models import ProApiTeams
 
 # Python Moving Average, taken by:
 # https://stackoverflow.com/questions/13728392/moving-average-or-running-mean
@@ -21,13 +22,12 @@ season_year = 2017
 team_labels = [team_abbreviation(i) for i in range(1,30)]
 
 #Team ID
-#Atlantic division ids are 2, 3, 20, 23, 28.
-#Southeast: 1, 16, 22, 30, 4
-#Southwest: 10, 7, 11, 27, 15
-#Pacific: 10, 13, 14, 24, 26
-#Noethwest: 8, 18, 25, 21, 29
-#Central: 6, 5, 17, 12, 9
-division_team_id_list = [10,13,14,24,26]
+#Possible divisions are Southeast, Atlantic, Central
+#Pacific, Southwest, Northwest
+division_name = "Central"
+query = ProApiTeams.select().where(ProApiTeams.division == division_name)
+division_team_id_list = [i.bball_ref for i in query]
+
 
 #Odds calculations
 odds_list = []
@@ -47,7 +47,7 @@ while b < datetime(season_year,4,15):
     
 odds_array = np.asarray(odds_list)
 
-plt.figure(figsize=(7.5,7.5))
+plt.figure(figsize=(5,5))
 plt.ylim(0,105) #so 100 shows up on the graph.
 
 #Get team data
@@ -59,10 +59,11 @@ for team_id_db in division_team_id_list:
     average_team_data = running_mean(team_data,average_count)
     average_dates_list = dates_list[average_count-1:]
     #plt.plot(dates_list,team_data)
-    plt.plot(average_dates_list,average_team_data, label=team_abbreviation(team_id+1))
+    plt.plot(average_dates_list,average_team_data, label=team_abbreviation(team_id+1), alpha = 0.6)
 
 plt.xlabel('Date')
 plt.ylabel('Team Playoff Odds')
-plt.title('Atlantic Division Playoff Odds '+str(season_year-1)+'-'+str(season_year))
+plt.title(division_name+' Division Playoff Odds '+str(season_year-1)+'-'+str(season_year))
 plt.legend()
+plt.xticks(rotation=25)
 plt.show()
