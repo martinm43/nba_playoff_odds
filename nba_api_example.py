6e8dc669a87c60343019676b39be6e91d2b62e08
miller.m.43@gmail.com <miller.m.43@gmail.com>
2018-10-18 11:34:28 -0400

    added team dictionary functionLity

--- nba_api_example.py
+++ nba_api_example.py
@@ -4,4 +4,6 @@ from datetime import datetime
 
 s = ScoreboardV2(game_date=datetime(2018,10,16))
 
-s.get_dict()
+day_results=s.line_score.get_dict()
+
+team_dicts=[dict(zip(day_results['headers'],d)) for d in day_results['data']]
