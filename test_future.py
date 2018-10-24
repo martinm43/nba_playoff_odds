de7a87008c64a98f47e935d923c1ff1fd1bdebe9
martin <MA Miller>
2018-10-23 20:49:49 -0400

    commenting test_future file

--- test_future.py
+++ test_future.py
@@ -1,14 +1,17 @@
 # coding: utf-8
 from prediction_table import playoff_odds_calc, playoff_odds_print
 from datetime import datetime
-start_datetime=datetime(2017,10,1)
-end_datetime=datetime(2018,5,1)
-season_year=2018
-input_predict_date=datetime(2017,1,23)
-input_season_year=2018
+
+start_datetime=datetime(2017,10,1) #start of data gathering period
+end_datetime=datetime(2018,1,1) #end of data gathering period
+season_year=2018 #year where data is gathered
+input_predict_date=datetime(2016,1,23) #when to start the season being predicted
+input_season_year=2017 #the season being predicted
+
 results=playoff_odds_calc(start_datetime,end_datetime,\
         season_year,input_predict_date=input_predict_date,\
         input_season_year=input_season_year,auto='OFF')
+
 results_table = playoff_odds_print(results)
 print("Playoff odds for the "+str(season_year)+" season as of "+end_datetime.strftime("%b %d %Y"))
 print(results_table)
