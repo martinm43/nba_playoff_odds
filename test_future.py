373af58d5e409d3b06781fd07dc39fdc890d5ea4
miller.m.43@gmail.com <miller.m.43@gmail.com>
2018-10-23 20:14:24 -0400

    Testing out some analytics functionality/resolving import issues

--- test_future.py
+++ test_future.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 from prediction_table import playoff_odds_calc, playoff_odds_print
 from datetime import datetime
-start_datetime=datetime(2018,2,1)
+start_datetime=datetime(2017,10,1)
 end_datetime=datetime(2018,5,1)
 season_year=2018
-input_predict_date=datetime(2017,10,1)
+input_predict_date=datetime(2017,10,23)
 input_season_year=2018
 results=playoff_odds_calc(start_datetime,end_datetime,\
         season_year,input_predict_date=input_predict_date,\
