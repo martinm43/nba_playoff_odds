8538f2b5746178437784427c907e572580fc4932
martin <MA Miller>
2018-11-10 18:02:43 -0500

    re-uploading 2018 NBA data into the database

--- info_table.py
+++ info_table.py
@@ -14,9 +14,9 @@ from analytics.pythag import pythagorean_wins, league_pythagorean_wins
 from wins_script import get_wins
 
 #Query Testing
-start_datetime = datetime(2017,10,01)
-end_datetime = datetime(2018,4,10)
-season_year = 2018
+start_datetime = datetime(2015,10,01)
+end_datetime = datetime(2016,4,10)
+season_year = 2016
 games_list=games_query(start_datetime,end_datetime)
 
 #Custom SRS calculation options
