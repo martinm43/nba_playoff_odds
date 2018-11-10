f859f32fa610149a45d39da674193ada663eef7d
martin <MA Miller>
2018-11-10 13:19:33 -0500

    Updated status table to provide information regarding actual wins and losses/updated wins script to use dates

--- info_table.py
+++ info_table.py
@@ -15,14 +15,17 @@ from wins_script import get_wins
 
 #Query Testing
 start_datetime = datetime(2018,10,01)
-end_datetime = datetime(2018,11,01)
-
+end_datetime = datetime(2018,11,10)
+season_year = 2019
 games_list=games_query(start_datetime,end_datetime)
 
 #Custom SRS calculation options
 max_MOV = 10
 home_team_adv = 2.5
 win_floor = 5
+
+wins_dict_list = [get_wins(i,season_year,start_datetime,end_datetime) for i in range(1,31)]
+wins_list = [[x['away_record'],x['home_record'],x['record']] for x in wins_dict_list] 
 
 #Pythagorean Wins
 lpw_results = league_pythagorean_wins(Game,mincalcdatetime=epochtime(start_datetime),\
@@ -32,11 +35,12 @@ srs_list = SRS(games_list, max_MOV = max_MOV, home_team_adv = home_team_adv, win
 
 lpw_results.sort(key = lambda x:x[0])
 
-results = zip(lpw_results,srs_list)
+#results = zip(lpw_results,srs_list)
+results = zip(lpw_results,srs_list,wins_list)
+results = [[x[0][0],x[0][1],x[1],x[2][0],x[2][1],x[2][2]] for x in results]
 
-results = [[x[0][0],x[0][1],x[1]] for x in results]
-
-results_tuples = [(team_abbreviation(x[0]),round(x[1],0),round(x[2]*100.0/100.0,3)) for x in results]
+results_tuples = [(team_abbreviation(x[0]),round(x[1],0),round(x[2]*100.0/100.0,3),\
+                   x[3],x[4],x[5]) for x in results]
 
 results_tuples.sort(key = lambda x:-x[2])
 
@@ -45,11 +49,14 @@ results_table = tabulate(
         headers=[
             'Team',
             'Pythag. Wins',
-            'Est. SRS'],
+            'Est. SRS',
+            'Away Record',
+            'Home Record',
+            'Overall Record'],
         tablefmt='rst',
         numalign='left')
 
-print("Pythagorean Win Expectations \n"+\
+print("Pythagorean Win Expectations, Est. SRS, and Records \n"+\
         "Based on Games Played Between: "+start_datetime.strftime('%b %d %Y')+\
         " and "+end_datetime.strftime('%b %d %Y'))
 print(results_table)
--- wins_script.py
+++ wins_script.py
@@ -1,12 +1,21 @@
 # coding: utf-8
-def get_wins(team_id,season_year):
+def get_wins(team_id,season_year,start_datetime,end_datetime):
     from nba_database.nba_data_models import BballrefScores as BS
-    from nba_database.queries import team_abbreviation
-    away_query = BS.select().where(BS.season_year == season_year, BS.away_team_id == team_id, BS.away_pts > 0)
+    from nba_database.queries import team_abbreviation, epochtime
+    away_query = BS.select().where(BS.season_year == season_year,\
+                          BS.away_team_id == team_id,\
+                          BS.away_pts > 0,\
+                          BS.datetime >= epochtime(start_datetime),\
+                          BS.datetime <= epochtime(end_datetime))
     away_results = [[i.away_pts, i.home_pts] for i in away_query]
     away_wins_total = sum([1 if x[0] > x[1] else 0 for x in away_results])
     away_games_total = len(away_results)
-    home_query = BS.select().where(BS.season_year == season_year, BS.home_team_id == team_id, BS.home_pts > 0)
+    #home_query = BS.select().where(BS.season_year == season_year, BS.home_team_id == team_id, BS.home_pts > 0)
+    home_query = BS.select().where(BS.season_year == season_year,\
+                          BS.home_team_id == team_id,\
+                          BS.away_pts > 0,\
+                          BS.datetime >= epochtime(start_datetime),\
+                          BS.datetime <= epochtime(end_datetime))
     home_results = [[i.home_pts, i.away_pts] for i in home_query]
     home_wins_total = sum([1 if x[0] > x[1] else 0 for x in home_results])
     home_games_total = len(home_results)
