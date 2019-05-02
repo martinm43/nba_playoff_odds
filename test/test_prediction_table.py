35ee980f1a1242c8fda239c974471e72220ed7bf
root <root@Owner-PC.localdomain>
2019-05-02 00:11:18 -0400

    adding descriptions of test scripts

---
+++ test/test_prediction_table.py
@@ -0,0 +1,2 @@
+# Compare the output table teams to the final list of teams that made
+# the playoffs? Or check an expected output list for several given years.
---
+++ test/test_update_nba_api.py
@@ -0,0 +1,2 @@
+# Select several dates, and compare the output json
+# for those dates to the expected json for those dates.
