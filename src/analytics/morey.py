d31b9c284eea712f6b6fa27b4bde016672dacd5a
MA Miller <miller.m.43@gmail.com>
2020-11-25 00:47:40 +0000

    Removing unused Burke logistic regression

--- src/analytics/morey.py
+++ src/analytics/morey.py
@@ -1,16 +1,8 @@
 #Selection of mathematical models for NBA win predictions
 
-def pts_regress(dpts):
-  import math
-  return 1/(1+math.exp(-1*(0.15+dpts*0.2)))
-
 def SRS_regress(dSRS):
   import math
   return 1/(1+math.exp(-1*(0.15+dSRS*.095))) #.35
 
-def burke_regress(dBurke):
-	import math
-	return 1/(1+math.exp(-1*(0.15+dBurke*0.2)))
-  
 if __name__=='__main__':
   print((SRS_regress(2)))
