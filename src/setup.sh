1553e2261646e94634ee42b710061c68fc0f77a7
miller.m.43@gmail.com <miller.m.43@gmail.com>
2020-12-25 22:56:25 -0500

    removing unused files

--- src/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export FLASK_APP=flask_matplotlib.py
-export FLASK_ENV=development
-flask run
--- src/test/test_plot_playoff_odds.py
+++
@@ -1,4 +0,0 @@
-# Follow outlines for unit testing
-# as given by
-# https://matplotlib.org/devel/testing.html
-# and using the sample images
