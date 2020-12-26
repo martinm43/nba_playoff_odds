d07842785268d3ea31881da16a969f74d877df77
MA Miller <miller.m.43@gmail.com>
2020-12-26 09:51:35 -0500

    added docstring to extract_SR_data.py file, including necessary header format removed unneeded season_data_2020.csv file

--- src/extract_SR_data.py
+++ src/extract_SR_data.py
@@ -2,6 +2,18 @@
  For parsing .csv files obtained from the Sports-Reference website.
  All data belongs to Sports Reference LLC themselves, an excellent site you 
  should consider supporting if you like this stuff.
+ 
+ Inputs:
+     season_year_start - year in which the "season year" starts (nominally)
+     filename of csv containing Sports Reference data
+     csv must have the following header format:
+         
+     Date,Start (ET),Visitor/Neutral,Visitor_PTS,Home/Neutral,Home_PTS,,,Attend.,Notes
+     
+     which must be modified from the following default header
+     
+     Date,Start (ET),Visitor/Neutral,PTS,Home/Neutral,PTS,,,Attend.,Notes
+     
 """
 from pprint import pprint
 import pandas as pd
@@ -10,8 +22,8 @@ from datetime import datetime
 from nba_database.queries import epochtime, full_name_to_id
 from nba_database.nba_data_models import database, BballrefScores
 
-season_year = 1989
-df = pd.read_csv('season_data_89-90.csv')
+season_year_start = 2019
+df = pd.read_csv('season_data_2020.csv')
 season_dicts = df.T.to_dict().values()
 
 print(len(season_dicts))
@@ -24,24 +36,19 @@ for d in season_dicts:
     #pprint(d)
     d['home_team'] = d['Home/Neutral']
     d['away_team'] = d['Visitor/Neutral']
-    d['home_pts'] = d['Home_PTS']
     d['away_pts'] = d['Visitor_PTS']
     d['home_team_id'] = full_name_to_id(d['Home/Neutral'])
     d['away_team_id'] = full_name_to_id(d['Visitor/Neutral'])
     d['date'] = d['Date']
-    d['season_year'] = season_year+1
-    #d['start_time'] = d['Start (ET)'] -- newer seasons only
+    d['season_year'] = season_year_start+1
 
-    #date conversion - newer seasons
-    #datestr = d['Date'] + ' ' + d['Start (ET)']+ 'm'
-    #datefmt = '%a %b %d %Y %H:%M%p'
-    #date conversion - older seasons
+    #standardized date conversion
     datestr = d['Date']
     datefmt = '%a %b %d %Y'
     date_datetime = datetime.strptime(datestr,datefmt)
     d['datetime'] = epochtime(date_datetime)
 
-
+    #remove unused keys
     d.pop('Visitor/Neutral',None)
     d.pop('Home/Neutral',None)
     d.pop('Attend.',None)
@@ -56,15 +63,16 @@ for d in season_dicts:
     d.pop('Unnamed: 6',None)
     d.pop('Unnamed: 7',None)
 
-    d['id'] = season_year*10000 + id
+    d['id'] = season_year_start*10000 + id
     id+=1
 
 season_dicts = list(season_dicts)
+print("Sample dict to be entered:")
 pprint(season_dicts[0])
 
 with database.atomic() as txn:
-     size = (SQLITE_MAX_VARIABLE_NUMBER // len(season_dicts[0])) - 1 
-     # remove one to avoid issue if peewee adds some variable
-     for i in range(0, len(season_dicts), size):
-        BballrefScores.insert_many(season_dicts[i:i+size]).on_conflict_replace().execute()
+      size = (SQLITE_MAX_VARIABLE_NUMBER // len(season_dicts[0])) - 1 
+      # remove one to avoid issue if peewee adds some variable
+      for i in range(0, len(season_dicts), size):
+         BballrefScores.insert_many(season_dicts[i:i+size]).on_conflict_replace().execute()
   
