fe182b5b15e3a31c7383e3352190be344e54b2ce
MA Miller <miller.m.43@gmail.com>
2020-12-26 10:37:46 -0500

    added docstring to points analysis file and removed unnecessary plot

--- src/points_analysis.py
+++ src/points_analysis.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 13 22:03:48 2020
-
-@author: martin
-
 Script for calculating what the "difference over sum" distribution looks like
 for the NBA, for later use in mathematical modelling.
+
+Inputs:
+    None (reads from database)
+    
+Outputs:
+    None (prints mean and standard deviation of logistic distribution fitted)
+
 """
 import pandas as pd
 import numpy as np
@@ -16,10 +19,11 @@ from sklearn.preprocessing import StandardScaler
 
 from nba_database.nba_data_models import BballrefScores
 
-# not ignoring warnings
-# warnings.filterwarnings("ignore")
-min_val = 1990
-max_val = 2020
+# Get minimum and maximum years for the distribution fitting
+x = BballrefScores.select().order_by(BballrefScores.season_year.asc()).get()
+min_val = x.season_year
+x = BballrefScores.select().order_by(BballrefScores.season_year.desc()).get()
+max_val = x.season_year
 
 z = BballrefScores.select().where(BballrefScores.season_year >= min_val, BballrefScores.season_year < max_val)
 mov = [x.home_pts-x.away_pts for x in z]
@@ -46,9 +50,7 @@ stddev = parameters[1]
 mean_str = "%.5f" % mean
 stddev_str = "%.5f" % stddev
 
+print("Result of logistic curve fitting for data from "+str(min_val)+\
+      " to "+str(max_val))
 print("logistic mean: "+mean_str)
 print("logistic stddev: "+stddev_str)
-
-cdf_x = np.linspace(-0.3,0.3,300)
-z_default = 2*stats.logistic.cdf(cdf_x,mean,stddev)-1
-plt.plot(cdf_x,z_default)
