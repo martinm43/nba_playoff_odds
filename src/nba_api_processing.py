2e9d0bad96533d8ced7a24b73466005f0baa8af5
MA Miller <miller.m.43@gmail.com>
2025-11-20 21:21:52 -0500

    Updated nba_api_scoreboard to use v3

--- src/nba_api_processing.py
+++ src/nba_api_processing.py
@@ -8,62 +8,61 @@ using automation
 import json
 
 from datetime import datetime, timedelta
-from pprint import pprint
 from nba_database.nba_data_models import BballrefScores
 from nba_database.queries import abbrev_to_id
 
-from nba_api.stats.endpoints import scoreboardv2
+from nba_api.stats.endpoints import scoreboardv3
 
 
 
-start_date = datetime.today() - timedelta(days=5)
+start_date = datetime.today() - timedelta(days=30)
 end_date = datetime.today()
 loop_date = start_date
 
 while loop_date < end_date:
-    
+
     game_date = loop_date.strftime("%Y-%m-%d")
     print("Processing date "+game_date) 
-    games = scoreboardv2.ScoreboardV2(game_date=game_date)
+    games = scoreboardv3.ScoreboardV3(game_date=game_date)
     games_json=json.loads(games.get_json())
+    #fname = f"responses/{datetime.now().isoformat()}.json"
+    #with open(fname, "w") as f:
+    #    json.dump(games_json, f, indent=2)
     
+    #pprint(games_json)
+    z=games_json.keys()
     # Reforming dicts of required data based on game results, identified by team abbrev
+    #print(games_json["scoreboard"].keys())
+    datalist = games_json["scoreboard"]["games"]
     
-    datalist = games_json["resultSets"][1]["rowSet"]
-    
-    print(datalist)
+    #print(datalist)
     results = []
         
     game_list = []
-    #Master list for determining what is in the api 
-    #7: record entering the game
-    #8,9,10,11: 1q 2q 3q 4q
-    #23: FG%
-    #24: FT%
-    #25: 3P%
-    #26: total assists
-    #27: total rebounds
-    #28: blocks
-    for i in range(0, len(datalist), 2):  # Step by 2 to process each pair
+    
+    for i in range(0, len(datalist)):  # Step by 2 to process each pair
+        to_proc = datalist[i]
         game = {
-            "away_abbrev": datalist[i][4],
-            "home_abbrev": datalist[i+1][4],
-            "away_team_id": abbrev_to_id(datalist[i][4]),           # Away team data
-            "home_team_id": abbrev_to_id(datalist[i + 1][4]),        # Home team data
-            "away_pts": datalist[i][22],           # Away team data
-            "home_pts": datalist[i + 1][22],        # Home team data
-            "game_date": datalist[i][0][0:10],           # Away team data
+            "away_abbrev": to_proc['awayTeam']['teamTricode'],
+            "home_abbrev": to_proc['homeTeam']['teamTricode'],
+            "away_team_id": abbrev_to_id(to_proc['awayTeam']['teamTricode']),           # Away team data
+            "home_team_id": abbrev_to_id(to_proc['homeTeam']['teamTricode']),        # Home team data
+            "away_pts": to_proc['awayTeam']['score'],           # Away team data
+            "home_pts": to_proc['homeTeam']['score'],        # Home team data
+            "game_date": datetime.fromisoformat(to_proc['gameEt'].replace("Z","+00:00")).strftime('%Y-%m-%d'),           # Away team data
         }
-        print(game["away_abbrev"]+" "+str(game["away_pts"])+", "+game["home_abbrev"]+" "+str(game["home_pts"]))
+        print(game["away_abbrev"]+" "+str(game["away_pts"])+", "+game["home_abbrev"]+" "+str(game["home_pts"])+" on "+game["game_date"])
         game_list.append(game)
-
-        
+    
+            
     for z in game_list: 
         BballrefScores.update(away_pts = z["away_pts"], home_pts = z["home_pts"]).\
             where((BballrefScores.date == z["game_date"]) & \
-                  (BballrefScores.away_team_id == z["away_team_id"]) & \
-                  (BballrefScores.home_team_id == z["home_team_id"])).execute()
-
+                    (BballrefScores.away_team_id == z["away_team_id"]) & \
+                    (BballrefScores.home_team_id == z["home_team_id"])).execute()
+    
         
     print("Processing date "+game_date+" complete.") 
     loop_date = loop_date + timedelta(days=1)
+
+
